16d173c073dd5eccc1ace1d67d12c1b5f8a8f89c
hyvuz <haneennasser1425@outlook.com>
2025-06-14 23:40:52 +0300

    Fix styling  error

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    #Uppercase: odd Ascii, lowercase: even Ascii 
+    # Uppercase: odd Ascii, lowercase: even Ascii 
     print("{}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end="")
