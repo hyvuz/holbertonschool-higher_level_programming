01faca27002cc56579e8d22b428497cb4c9bc071
hyvuz <haneennasser1425@outlook.com>
2025-06-15 00:24:57 +0300

    Fix  error

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,4 +4,5 @@ from add_0 import add  # Import the add function from the module (add_0)
 a = 1                  # Define a in a line
 b = 2                  # Define b in another line
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
