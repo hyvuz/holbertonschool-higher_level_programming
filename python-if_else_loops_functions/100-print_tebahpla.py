601d680a5ccc910cab6c07770f501f0839600f02
hyvuz <haneennasser1425@outlook.com>
2025-06-14 23:30:42 +0300

    Fix error

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    #Uppercase: even Ascii, lowercase: odd Ascii 
-    print("{}".format(chr(i - 32) if i % 2 == 0 else chr(i)), end="")
+    #Uppercase: odd Ascii, lowercase: even Ascii 
+    print("{}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end="")
