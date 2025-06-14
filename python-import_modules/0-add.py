86b08b9d9413e90156f82d71c47c0c0326f5097d
hyvuz <haneennasser1425@outlook.com>
2025-06-15 00:16:43 +0300

    Add 0-add.py

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from add_0 import add  # Import the add function from the module (add_0)
+
+a = 1                  # Define a in a line
+b = 2                  # Define b in another line
+
+print("{} + {} = {}".format(a, b, add(a, b)))
