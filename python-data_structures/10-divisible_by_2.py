f8269cd8c0fb680e58e05e0ee7489f62b70125bc
hyvuz <haneennasser1425@outlook.com>
2025-06-25 22:27:55 +0300

    Update 10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     div_2 = []
 
     for i in my_list:
-        if my_list[i] % 2 == 0:
+        if i % 2 == 0:
             div_2.append(True)
         else:
             div_2.append(False)
