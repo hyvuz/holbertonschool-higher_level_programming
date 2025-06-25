0e3114fd1ef268711c1305c177e213b45028d831
hyvuz <haneennasser1425@outlook.com>
2025-06-25 21:38:21 +0300

    Add 8-multiple_returns.py

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    sen_length = len(sentence)
+    
+    if sen_length == 0:
+        my_tuple = (sen_length, None)
+    
+    else :
+        my_tuple = (sen_length, sentence[0])
+        
+    return my_tuple
