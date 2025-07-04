f7495bf0c71ac2d414ebefc4a19d024255da9a18
hyvuz <haneennasser1425@outlook.com>
2025-07-05 01:53:22 +0300

    Fix styling error

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+
     if not isinstance(roman_string, str) or not roman_string:
         return 0
 
-    roman_map = {
+    roman_list = {
         'I': 1,
         'V': 5,
         'X': 10,
@@ -11,12 +12,13 @@ def roman_to_int(roman_string):
         'C': 100,
         'D': 500,
         'M': 1000
-    } 
-    
+    }
+
     total = 0
     length = len(roman_string)
     for i in range(length):
         current = roman_list.get(roman_string[i], 0)
+
         next_value = roman_list.get(roman_string[i + 1], 0) if i + 1 < length else 0
 
         if current < next_value:
