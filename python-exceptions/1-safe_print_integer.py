cef562f798e0cab57507025a0449e71fd52acb03
hyvuz <haneennasser1425@outlook.com>
2025-07-06 20:20:27 +0300

    Fix some errors

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except(ValueError, TypeError):
         return True
-    else:
+    except (ValueError, TypeError):
         return False
