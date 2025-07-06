21554d1b6635a71abe392ce188efc4c3eaf2fcfc
hyvuz <haneennasser1425@outlook.com>
2025-07-06 21:56:07 +0300

    Add 0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+0-add_integer module.
+
+Provides a function to add two integers (with float inputs truncated),
+and raises TypeError on invalid types.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100, -2)
+    98
+    >>> add_integer(2)
+    100
+    >>> add_integer(100.3, -2)
+    98
+    >>> add_integer(4, "School")
+    Traceback (most recent call last):
+      ...
+    TypeError: b must be an integer
+    >>> add_integer(None)
+    Traceback (most recent call last):
+      ...
+    TypeError: a must be an integer
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
