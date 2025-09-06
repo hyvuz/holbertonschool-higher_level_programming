acaa6caea119b41a831239f2440aa069824058e6
hyvuz <haneennasser1425@outlook.com>
2025-09-06 16:46:09 +0300

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a Square class with size validation and area calculation."""
+"""Defines a Square class with size validation."""
 
 class Square:
     """Represents a square."""
