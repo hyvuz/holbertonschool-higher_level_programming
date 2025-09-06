15d8de1a0526bf75a2d29a6d4ea61f1c3bc0921f
hyvuz <haneennasser1425@outlook.com>
2025-09-07 00:29:24 +0300

    Update 5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
-""" Module Square """
-
+"""Defines a Square class with print functionality."""
 
 class Square:
-    """ Define a square by size """
+    """Represents a square."""
     def __init__(self, size=0):
-        self.__size = size
-
-    def area(self):
-        return self.__size ** 2
+        self.size = size
 
     @property
     def size(self):
+        """Retrieve the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) is not int:
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
 
     def my_print(self):
+        """Print the square with '#' characters."""
         if self.__size == 0:
-            print()
+            print("")
         else:
-            for i in range(self.size):
-                print("#" * self.size)
+            for i in range(self.__size):
+                print("#" * self.__size)
