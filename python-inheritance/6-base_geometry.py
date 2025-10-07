26ab982f29d43f43495b0559e3a73a52124f0d68
hyvuz <haneennasser1425@outlook.com>
2025-10-07 17:23:02 +0300

    Create 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry with method area that raises Exception
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class with area method"""
+
+    def area(self):
+        """Raises an Exception with a message"""
+        raise Exception("area() is not implemented")
