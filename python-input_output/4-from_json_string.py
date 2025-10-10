1d6f4c75664c81e306d09f814afa11bdf840f56d
hyvuz <haneennasser1425@outlook.com>
2025-10-11 01:14:12 +0300

    Create 4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that returns a Python object from a JSON string."""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object(Python data structure)represented by a JSON string"""
+    return json.loads(my_str)
