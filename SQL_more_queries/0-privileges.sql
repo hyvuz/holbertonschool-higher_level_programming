06b611982f44dbd16541da7633476fab009acf8b
hyvuz <haneennasser1425@outlook.com>
2025-11-02 01:42:59 +0300

    Add SQL script to show user privileges
    This script lists the privileges for two specific users on localhost.

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- List all privileges for user_0d_1 and user_0d_2 on localhost
+
+-- Grants for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Grants for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
